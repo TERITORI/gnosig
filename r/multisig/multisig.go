f3d455bdcf3659d5928b6c2b6bf9f44955c2711d
go7066 <strdev123strdev@gmail.com>
2022-11-01 20:08:08 +0800

    basic implementation of contract functions

--- r/multisig/multisig.go
+++ r/multisig/multisig.go
@@ -1,211 +1,172 @@
 package multisig
 
-// state
-// we multiply by this when calculating needed_votes in order to round up properly
-// Note: `10u128.pow(9)` fails as "u128::pow` is not yet stable as a const fn"
-const PRECISION_FACTOR = 1_000_000_000
+import (
+	"std"
 
-type Config struct {
-	Threshold       Threshold
-	MaxVotingPeriod uint64
-	// Total weight and voters are queried from this contract
-	// GroupAddr Cw4Contract
+	std "github.com/gnolang/gno/stdlibs/stdshim"
+)
+
+// MULTI SIG DATA TYPES
+type Approval struct {
+	address   std.Address // address of the approver
+	timestamp uint64      // block timestamp of the approval
 }
 
-type Group struct {
-	Members []Member
-}
+type ProposalStatus uint32
 
-type Member struct {
-	Address string
-	Power   uint64
-}
-
-type Threshold struct {
-	// /// Declares that a fixed weight of Yes votes is needed to pass.
-	// /// See `ThresholdResponse.AbsoluteCount` in the cw3 spec for details.
-	// AbsoluteCount { weight: u64 },
-
-	// /// Declares a percentage of the total weight that must cast Yes votes in order for
-	// /// a proposal to pass.
-	// /// See `ThresholdResponse.AbsolutePercentage` in the cw3 spec for details.
-	// AbsolutePercentage { percentage: Decimal },
-
-	// /// Declares a `quorum` of the total votes that must participate in the election in order
-	// /// for the vote to be considered at all.
-	// /// See `ThresholdResponse.ThresholdQuorum` in the cw3 spec for details.
-	// ThresholdQuorum { threshold: Decimal, quorum: Decimal },
-}
+const (
+	PENDING    ProposalStatus = 0
+	TO_EXECUTE ProposalStatus = 1
+	EXECUTED   ProposalStatus = 2
+	EXPIRED    ProposalStatus = 3
+	CANCELLED  ProposalStatus = 4
+)
 
 type Proposal struct {
-	Title       string
-	Description string
-	StartHeight uint64
-	Expires     uint64
-	Msgs        []Msg
-	Status      Status
-	// pass requirements
-	Threshold Threshold
-	// the total weight when the proposal started (used to calculate percentages)
-	TotalWeight uint64
-	// summary of existing votes
-	Votes Votes
+	Id          uint64         // incremented at every new proposal
+	Title       string         // title metadata
+	Description string         // description metadata
+	Expiration  uint64         // expiration timestamp
+	Tx          []Msg          // raw transaction
+	status      ProposalStatus // 0: PENDING | 1: TO_EXECUTE | 2: EXECUTED | 3: EXPIRED
+	Approvals   []Approval     // approvals on the proposal
 }
 
-type Status struct {
+type Quorum struct {
+	members []std.Address // members of the quorum
+	ratio   float64       // approval ratio of the quorum
 }
 
-// weight of votes for each option
-type Votes struct {
-	Yes     uint64
-	No      uint64
-	Abstain uint64
-	Veto    uint64
-}
+// MULTI SIG STATE
+var quorum Quorum
+
+var proposals []Proposal
 
 type Msg struct{}
 
-// impl Votes {
-//     /// sum of all votes
-//     pub fn total(&self) -> u64 {
-//         self.yes + self.no + self.abstain + self.veto
-//     }
+func isQuorumMember(address std.Address) bool {
+	for _, member := range quorum.members {
+		if member == address {
+			return true
+		}
+	}
+	return false
+}
 
-//     /// create it with a yes vote for this much
-//     pub fn yes(init_weight: u64) -> Self {
-//         Votes {
-//             yes: init_weight,
-//             no: 0,
-//             abstain: 0,
-//             veto: 0,
-//         }
-//     }
+// MULTI SIG FUNCTIONS
+func CreateProposal(title, description string, rawTx []Msg, expirationTimestamp uint64) {
+	// assert CallTx call.
+	std.AssertOriginCall()
+	caller := std.GetCallerAt(2)
+	if caller != std.GetOrigCaller() {
+		panic("should not happen") // because std.AssertOrigCall().
+	}
 
-//     pub fn add_vote(&mut self, vote: Vote, weight: u64) {
-//         match vote {
-//             Vote::Yes => self.yes += weight,
-//             Vote::Abstain => self.abstain += weight,
-//             Vote::No => self.no += weight,
-//             Vote::Veto => self.veto += weight,
-//         }
-//     }
-// }
+	// if sender is not in the current quorum, revert
+	isCallerMultisigMember := isQuorumMember(caller)
+	if !isCallerMultisigMember {
+		panic("caller is not a multisig member")
+	}
 
-// impl Proposal {
-//     /// current_status is non-mutable and returns what the status should be.
-//     /// (designed for queries)
-//     pub fn current_status(&self, block: &BlockInfo) -> Status {
-//         let mut status = self.status;
+	// TODO: if expirationTimestamp is already reached, panic
 
-//         // if open, check if voting is passed or timed out
-//         if status == Status::Open && self.is_passed(block) {
-//             status = Status::Passed;
-//         }
-//         if status == Status::Open && self.expires.is_expired(block) {
-//             status = Status::Rejected;
-//         }
+	// Create a proposal for the quorum and add it to the proposals array
+	proposalId := uint64(len(proposals))
+	proposals = append(proposals, Proposal{
+		Id:          proposalId,
+		Title:       title,
+		Description: description,
+		Expiration:  expirationTimestamp,
+		Tx:          rawTx,
+		status:      PENDING,      // 0: PENDING | 1: TO_EXECUTE | 2: EXECUTED | 3: EXPIRED
+		Approvals:   []Approval{}, // approvals on the proposal
+	})
+}
 
-//         status
-//     }
+func Approve(proposalId uint64, execute bool) {
+	// assert CallTx call.
+	std.AssertOriginCall()
+	caller := std.GetCallerAt(2)
+	if caller != std.GetOrigCaller() {
+		panic("should not happen") // because std.AssertOrigCall().
+	}
 
-//     /// update_status sets the status of the proposal to current_status.
-//     /// (designed for handler logic)
-//     pub fn update_status(&mut self, block: &BlockInfo) {
-//         self.status = self.current_status(block);
-//     }
+	// if sender is not in the current quorum, revert
+	isCallerMultisigMember := isQuorumMember(caller)
+	if !isCallerMultisigMember {
+		panic("caller is not a multisig member")
+	}
 
-//     // returns true iff this proposal is sure to pass (even before expiration if no future
-//     // sequence of possible votes can cause it to fail)
-//     pub fn is_passed(&self, block: &BlockInfo) -> bool {
-//         match self.threshold {
-//             Threshold::AbsoluteCount {
-//                 weight: weight_needed,
-//             } => self.votes.yes >= weight_needed,
-//             Threshold::AbsolutePercentage {
-//                 percentage: percentage_needed,
-//             } => {
-//                 self.votes.yes
-//                     >= votes_needed(self.total_weight - self.votes.abstain, percentage_needed)
-//             }
-//             Threshold::ThresholdQuorum { threshold, quorum } => {
-//                 // we always require the quorum
-//                 if self.votes.total() < votes_needed(self.total_weight, quorum) {
-//                     return false;
-//                 }
-//                 if self.expires.is_expired(block) {
-//                     // If expired, we compare Yes votes against the total number of votes (minus abstain).
-//                     let opinions = self.votes.total() - self.votes.abstain;
-//                     self.votes.yes >= votes_needed(opinions, threshold)
-//                 } else {
-//                     // If not expired, we must assume all non-votes will be cast as No.
-//                     // We compare threshold against the total weight (minus abstain).
-//                     let possible_opinions = self.total_weight - self.votes.abstain;
-//                     self.votes.yes >= votes_needed(possible_opinions, threshold)
-//                 }
-//             }
-//         }
-//     }
-// }
+	// if sender already approved this proposal, panic
+	if int(proposalId) >= len(proposals) {
+		panic("invalid proposal id")
+	}
+	proposal := proposals[proposalId]
+	for _, approval := range proposal.Approvals {
+		if approval.address == caller {
+			panic("already approved")
+		}
+	}
 
-// // this is a helper function so Decimal works with u64 rather than Uint128
-// // also, we must *round up* here, as we need 8, not 7 votes to reach 50% of 15 total
-// fn votes_needed(weight: u64, percentage: Decimal) -> u64 {
-//     let applied = percentage * Uint128::new(PRECISION_FACTOR * weight as u128);
-//     // Divide by PRECISION_FACTOR, rounding up to the nearest integer
-//     ((applied.u128() + PRECISION_FACTOR - 1) / PRECISION_FACTOR) as u64
-// }
+	// TODO: if proposal expiration time is reached, set proposal as EXPIRED
+	proposals[proposalId].status = EXPIRED
 
-// // we cast a ballot with our chosen vote and a given weight
-// // stored under the key that voted
-// #[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
-// pub struct Ballot {
-//     pub weight: u64,
-//     pub vote: Vote,
-// }
+	// Create an approval in a the specified proposal
+	proposals[proposalId].Approvals = append(proposal.Approvals, Approval{
+		address:   caller,
+		timestamp: 0, // TODO: set from block timestamp
+	})
 
-// // unique items
-// pub const CONFIG: Item<Config> = Item::new("config");
-// pub const PROPOSAL_COUNT: Item<u64> = Item::new("proposal_count");
+	if float64(quorum.members)*float64(quorum.ratio) >= float64(len(proposal.Approvals)) {
+		if execute { // if current quorum is reached after this vote on the proposal and execute is true, execute the proposal
+			Execute(proposalId)
+		} else { // if current quorum is reached after this vote on the proposal and execute is false, set proposal to TO_EXECUTE
+			proposals[proposalId].status = TO_EXECUTE
+		}
+	}
+}
 
-// // multiple-item map
-// pub const BALLOTS: Map<(u64, &Addr), Ballot> = Map::new("votes");
-// pub const PROPOSALS: Map<u64, Proposal> = Map::new("proposals");
+func Execute(proposalId uint64) {
+	// assert CallTx call.
+	std.AssertOriginCall()
+	caller := std.GetCallerAt(2)
+	if caller != std.GetOrigCaller() {
+		panic("should not happen") // because std.AssertOrigCall().
+	}
 
-// pub fn next_id(store: &mut dyn Storage) -> StdResult<u64> {
-//     let id: u64 = PROPOSAL_COUNT.may_load(store)?.unwrap_or_default() + 1;
-//     PROPOSAL_COUNT.save(store, &id)?;
-//     Ok(id)
-// }
+	if int(proposalId) >= len(proposals) {
+		panic("invalid proposal id")
+	}
+	proposal := proposals[proposalId]
 
-// pub fn parse_id(data: &[u8]) -> StdResult<u64> {
-//     match data[0..8].try_into() {
-//         Ok(bytes) => Ok(u64::from_be_bytes(bytes)),
-//         Err(_) => Err(StdError::generic_err(
-//             "Corrupted data found. 8 byte expected.",
-//         )),
-//     }
-// }
+	// if tx is not TO_EXECUTE, revert
+	if proposal.status != TO_EXECUTE {
+		panic("not an executable proposal")
+	}
 
-// // commands
-//     Propose {
-//         title: String,
-//         description: String,
-//         msgs: Vec<CosmosMsg<Empty>>,
-//         // note: we ignore API-spec'd earliest if passed, always opens immediately
-//         latest: Option<Expiration>,
-//     },
-//     Vote {
-//         proposal_id: u64,
-//         vote: Vote,
-//     },
-//     Execute {
-//         proposal_id: u64,
-//     },
-//     Close {
-//         proposal_id: u64,
-//     },
-//     /// Handles update hook messages from the group contract
-//     MemberChangedHook(MemberChangedHookMsg),
+	// if sender is not in the current quorum, revert
+	isCallerMultisigMember := isQuorumMember(caller)
+	if !isCallerMultisigMember {
+		panic("caller is not a multisig member")
+	}
+
+	// TODO: if expiration time is reached, set proposal as CANCELLED
+	proposals[proposalId].status = CANCELLED
+
+	// TODO: Execute an approved tx
+
+	// set proposal to EXECUTED
+	proposals[proposalId].status = EXECUTED
+}
+
+func updateQuorum(addresses []std.Address, ratio float64) {
+	// TODO: if sender is not the multisig calling itself, revert
+
+	// Update the current quorum
+	// Note: to update the quorum, users have to make a proposal with rawTx calling this internal func
+	quorum.members = addresses
+	quorum.ratio = ratio
+}
 
 // // query
 // 	/// Return ThresholdResponse
